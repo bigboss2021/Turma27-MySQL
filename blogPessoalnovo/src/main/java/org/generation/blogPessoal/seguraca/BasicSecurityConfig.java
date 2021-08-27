c96ee293437bda0fe09cef667f6affc363cc43e5
bigboss2021 <limavalmir2000@hotmail.com>
2021-08-27 18:04:59 -0300

    imagens

--- blogPessoalnovo/src/main/java/org/generation/blogPessoal/seguraca/BasicSecurityConfig.java
+++ blogPessoalnovo/src/main/java/org/generation/blogPessoal/seguraca/BasicSecurityConfig.java
@@ -20,7 +20,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
-
+		auth.inMemoryAuthentication().withUser("root").password(passwordEncoder().encode("root"))
+				.authorities("ROLE_USER");
 	}
 
 	@Bean
@@ -31,8 +32,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar").permitAll()
-				.anyRequest().authenticated().and().httpBasic().and().sessionManagement()
+		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar")
+				.permitAll().anyRequest().authenticated().and().httpBasic().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and().csrf().disable();
 
 	}
